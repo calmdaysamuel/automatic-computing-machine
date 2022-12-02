bb6dbc0063baa8c816381bd65b77fad2f33ccc19
SUBOMI123 <Oluwasubomibashoru@bison.howard.edu>
2022-12-02 15:21:12 -0500

    added css to the login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,10 @@
+import "./LoginPage.css";
 export function LoginPage() {
   return (
     <div className="lp">
-      <h1 className="lp-app-title">Quizly: A Trivia Game</h1>
-      <h2 className="lp-makers">By Samuel Calmday, Kevin Claiborne</h2>
+      <img src="Logo.png" alt="Flowers in Chania"> 
+      </img>
+      <h2 className="lp-makers">By Samuel Calmday, Kevin Claiborne, Oluwasubomi Bashorun</h2>
       <button className="lp-login">Login</button>
     </div>
   );
